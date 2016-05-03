7ad1a07d05d20718715ad57f72b6f74df84e7206
Vaggi <vaganmanuk@gmail.com>
2016-05-03 23:56:13 +0300

    soaring balls syndiacte almost finished

--- public/javascripts/soaring_balls_syndicate.js
+++ public/javascripts/soaring_balls_syndicate.js
@@ -11,40 +11,41 @@ return this.each(function () {
 
 
 $('.menu_big_item').children('a').click(function(){
+        $.fn.fullpage.setScrollingSpeed(0);
         $('.menu_big_toggle').show();
         $('.menu_big').removeClass('on');
-        var html = jQuery('html');
-        var scrollPosition = html.data('scroll-position');
-        html.css('overflow', html.data('previous-overflow'));
-        window.scrollTo(scrollPosition[0], scrollPosition[1])
 });     
-     
-$('.arrow_down_pure').click(function(){
-    $('#intro').scrollView();
-});   
+  
     
 $('.menu_big_toggle').click(function(){ 
-       $('.menu_big').addClass('on');
-        $('.menu_big_toggle').hide();
-       // lock scroll position, but retain settings for later
-        var scrollPosition = [
-          self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-          self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-        ];
-        var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
-        html.data('scroll-position', scrollPosition);
-        html.data('previous-overflow', html.css('overflow'));
-        html.css('overflow', 'hidden');
-        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+    $('.menu_big').addClass('on');
+    $('.menu_big_toggle').hide();
+});
+    
+$('.menu_big_toggle_black').click(function(){ 
+    $('.menu_big').addClass('on');
+    $('.menu_big_toggle').hide();
 });
     
 $('.menu_close').click(function(){
     $('.menu_big').removeClass('on');
     $('.menu_big_toggle').show();
-    var html = jQuery('html');
-    var scrollPosition = html.data('scroll-position');
-    html.css('overflow', html.data('previous-overflow'));
-    window.scrollTo(scrollPosition[0], scrollPosition[1])
 }); 
+    
+  $('#fullpage').fullpage({
+      anchors: ['home', 'intro1','intro2', 'works', 'clients', 'about', 'service', 'contact'],
+      menu: "#menu",
+      scrollingSpeed: 600,
+      normalScrollElements: '#menu',
+      'easingcss3':'cubic-bezier(.77,.11,.8,.41)',
+      afterLoad: function(){
+          $.fn.fullpage.setScrollingSpeed(600);
+      },
+      afterSlideLoad: function(){
+          $.fn.fullpage.setScrollingSpeed(600);
+      }
+  });
+
+
 
 });
