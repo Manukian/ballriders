8c11b707f2b544f0dbcc85c680f0e916d27d2292
Vaggi <vaganmanuk@gmail.com>
2016-05-05 23:50:30 +0300

    mobile menu done

--- public/javascripts/soaring_balls_syndicate.js
+++ public/javascripts/soaring_balls_syndicate.js
@@ -32,12 +32,23 @@ $('.menu_close').click(function(){
     $('.menu_big_toggle').show();
 }); 
     
+$('.topside_menu_mobile_toggle').click(function(){
+    $('.topside_menu_mobile').addClass('on');
+    $('.topside_menu_mobile_toggle').hide();
+});
+    
+$('.topside_menu_mobile_close').click(function(){
+    $('.topside_menu_mobile').removeClass('on');
+    $('.topside_menu_mobile_toggle').show();
+});
+    
   $('#fullpage').fullpage({
       anchors: ['home', 'intro1','intro2', 'works', 'clients', 'about', 'service', 'contact'],
       menu: "#menu",
       scrollingSpeed: 600,
-      normalScrollElements: '#menu',
+      normalScrollElements: '#menu, .topside_menu_mobile',
       'easingcss3':'cubic-bezier(.77,.11,.8,.41)',
+      verticalCentered: false,
       afterLoad: function(){
           $.fn.fullpage.setScrollingSpeed(600);
       },
