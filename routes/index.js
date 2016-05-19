eb14bed02b9c69ff5237ce3880af15a66a3adff6
Vaggi <vaganmanuk@gmail.com>
2016-05-19 14:27:05 +0300

    fin

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ballriders | Coming soon' });
-});
-router.get('/main', function(req, res, next) {
   res.render('main', { title: 'Ballriders | Main Page' });
 });
 router.get('/soaring_balls_syndicate', function(req, res, next) {
