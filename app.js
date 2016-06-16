a64144209764caae2aa90df2ba43a4cae59a62bb
Vaggi <vaganmanuk@gmail.com>
2016-06-17 02:34:57 +0300

    compression

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+var compress = require('compression');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var compress = require('compression');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
