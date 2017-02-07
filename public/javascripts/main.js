443e0fff2e2e6833d2269fb305b77f9ed16120d7
Vaggi <vaganmanuk@gmail.com>
2017-02-07 15:24:54 +0300

    mobile main page fixes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        $('.menu_block').addClass('menu_block_mobile');
-        $('body').css('overflow', 'visible');
-        var viewport = $(window).height();
-        $('.menu_block_mobile').height(viewport / 3);
-        $('.section_header').css('margin-top', viewport / 4 - 15 + 'px');
-        $('img').height(viewport / 4 - 10);
+        $('.menu_block').each(function(){
+            $(this).addClass('menu_block_mobile');
+            $(this).height($(window).height());
+            $('span.section_header').css('margin-top', ($(window).height()*0.5));
+            $('.menu_block-ballride_style').css('background-image','url(../images/people3.jpg)');
+            $('.menu_block-soaring_balls_syndicate').css('background-image','url(../images/ballhead.jpg)');
+            $('.menu_block-ballriders_creative_group').css('background-image','url(../images/IMG_1811_min.jpg)');
+            $('#vid').height(0);
+        });
     } else {
         var trail = $(window).width() / 10;
         $('.menu_block').mouseover(function() {
@@ -17,12 +20,12 @@ $(document).ready(function() {
             });
             if (elem.hasClass('menu_block-soaring_balls_syndicate')) {
                 elem.parent('main').children('.menu_block').each(function() {
-                    $(this).css('-webkit-transform', 'translateX(' + trail + 'px)');
+                    $(this).css('-webkit-transform', 'translateX(0px)');
                 });
             }
             if (elem.hasClass('menu_block-ballride_style')) {
                 elem.parent('main').children('.menu_block').each(function() {
-                    $(this).css('-webkit-transform', 'translateX(0px)');
+                    $(this).css('-webkit-transform', 'translateX(' + trail + 'px)');
                 });
             }
             if (elem.hasClass('menu_block-ballriders_creative_group')) {
