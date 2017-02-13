740057036ba0df5fb1cd80f32a6c87672e846425
Vaggi <vaganmanuk@gmail.com>
2017-02-13 10:34:40 +0300

    arrow in home page removed

--- public/javascripts/soaring_balls_syndicate.js
+++ public/javascripts/soaring_balls_syndicate.js
@@ -8,7 +8,4 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 $('#contact_section').children('.circle_wrapper').children('.arrow_pure').click(function () {
     $('#home_section').scrollView();
 });
-$('#home_section').children('.arrow_pure').click(function () {
-    $('#about_section').scrollView();
-});
 $('.menu_big_item_link').click(function(){$('.menu_big_toggle').show();$('.menu_big').removeClass('on');});$('.menu_big_toggle').click(function(){$('.menu_big').addClass('on');$('.menu_big_toggle').hide();});$('.menu_big_toggle_black').click(function(){$('.menu_big').addClass('on');$('.menu_big_toggle').hide();});$('.menu_close').click(function(){$('.menu_big').removeClass('on');$('.menu_big_toggle').show();});$('.topside_menu_mobile_toggle').click(function(){$('.topside_menu_mobile').addClass('on');$('.topside_menu_mobile_toggle').hide();});$('.topside_menu_mobile_close').click(function(){$('.topside_menu_mobile').removeClass('on');$('.topside_menu_mobile_toggle').show();});});
