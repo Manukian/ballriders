735002b1d7bcd89585b5a6d3bfc5aa50b1ac60dc
Vaggi <vaganmanuk@gmail.com>
2017-03-20 15:56:18 +0300

    little design fix

--- public/javascripts/works.js
+++ public/javascripts/works.js
@@ -1,4 +1,8 @@
-$(document).ready(function(){
+$(window).on('load', function () {
+    $preloader = $('.loader_area'),
+    $loader = $preloader.find('.loader');
+    $loader.delay(500).fadeOut('slow');
+    $preloader.delay(600).fadeOut('slow');
 
 var hands = $('#hands');
     
